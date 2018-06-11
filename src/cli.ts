69c71ece6e739b09cad960b5852202a6db778c82
Glenn Sonna <kenpachi.os@gmail.com>
2018-06-11 16:58:16 +0200

    Added tests for writClassFile

--- src/cli.ts
+++ src/cli.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import * as minimist from 'minimist';
 import { ParsedArgs } from 'minimist';
 
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,1 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import { PathLike } from 'fs';
-
-export const writeFile = (content: string, outPath: string) => {
-  const _generatedDirName = path.parse(outPath).dir;
-  if(_generatedDirName != './' && !fs.existsSync(_generatedDirName)) {
-    fs.mkdirSync(_generatedDirName);
-  }
-  return fs.writeFileSync(outPath, content)
-}
+export * from './fs-pythian';
